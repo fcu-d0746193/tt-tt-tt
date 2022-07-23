0e2dbb64fd1f3d0725761cf1987e5fb256d3fd8a
fcu-d0746193 <62371098+fcu-d0746193@users.noreply.github.com>
2022-07-24 06:34:10 +0800

    Update main_chatbot.py

--- main_chatbot.py
+++ main_chatbot.py
@@ -16,12 +16,12 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 app = Flask(__name__)
 
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
-#line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-#handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+#config = configparser.ConfigParser()
+#config.read('config.ini')
+#line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+#handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 status1 = 0
 status2 = 0
@@ -119,5 +119,5 @@ def handle_message(event):
         print("請先書入圖文選單功能後進行輸入")
 
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
